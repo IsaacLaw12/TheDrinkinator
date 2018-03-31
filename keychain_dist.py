0acc1a54495dd00c6c72555e60cb393a3b8eea71
Isaac Law <isaaclaw12@hotmail.com>
2018-03-31 13:10:54 -0600

    Create keychain_dist.py

---
+++ keychain_dist.py
@@ -0,0 +1,7 @@
+'''
+    This file contains a skeleton of a key dictionary.  Fill in the key values once deployed to an appropriate location.
+'''
+keychain = {
+  pn_subscribe_key = '',
+  pn_publish_key = '',
+}
