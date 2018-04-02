28cc17c38544c7f4b25d41f89be66dd400677a42
Isaac Law <isaaclaw12@hotmail.com>
2018-04-01 18:20:44 -0600

    Add files via upload
    Updated State to process some incoming requests.  Implemented request processing in drink_server. Similar
    functionality should be copied to the client to implement requests.

--- drink_helper.py
+++ drink_helper.py
@@ -7,32 +7,67 @@ Authors:
     Sabrina White
 '''
 import pickle
+import json
 
 class Recipe:
-    def __init__(self, recipe_name, option_one, option_two, option_three=None, option_four=None):
+    def __init__(self, recipe_name=None, options=None, recipe_dict=None):
         '''
         Initialize the recipe. A recipe only requires two drinks, the other two are optional
         Options are tuples and have the format (drink_name, amount). Amount is an integer
         value. 1 amount = 10 seconds of pumping or about 15 mL of fluid.
+        
+        If recipe_name is omitted then a recipe_dict can be passed in to recreate a recipe
+        instance from a saved dictionary.  This should be formatted in the same way as returned
+        by the dict_obj method.
+        Procedure for sending Recipe objects:
+        Get jsonifiable version of Recipe by calling:
+            Multiple - jsonable_recipes = [recipe.dict_obj() for name, recipe in State.recipes.items()]
+            Single - jsonable_recipes = [ recipe.dict_obj() ]
+        Receive json version of Recipe:
+            # Takes first tuple out of message_queue, and iterates through the recipe list at index 2
+            for recipe in Message_Handler.message_queue[0][2]: 
+                recreated = Recipe(recipe_dict=recipe)
+            
         '''
-        
-        # A temporaray list of drink options to loop over
-        options = [option_one, option_two, option_three, option_four]
-        
-        self.recipe_name = recipe_name
-        self.ingredients = {}
-        for opt in options:
-            if opt:
-                # Set the ingredient name in the dictionary and assign it the amount
-                self.ingredients[opt[0]] = opt[1]
-    
+        if recipe_name:
+            # Create new recipe from recipe_name and options
+            self.recipe_name = recipe_name
+            self.ingredients = {}
+            if len(options) < 2:
+                raise ValueError("Recipe requires at least two drinks. Not enough given: " + str(options))
+            for opt in options:
+                if opt:
+                    # Set the ingredient name in the dictionary and assign it the amount
+                    self.ingredients[opt[0]] = opt[1]
+        elif recipe_dict:
+            # Recreate Recipe from dictionary representation
+            #recipe_dict = json.loads(recipe_dict)[0]
+            print("Recipe dict is: " + str(recipe_dict))
+            # Create object from dictionary
+            if 'recipe_name' not in recipe_dict:
+                raise KeyError("Recipe cannot be created from incorrectly formatted dictionary")
+            self.recipe_name = recipe_dict['recipe_name']
+            self.ingredients = {}
+            for key in recipe_dict:
+                if key is not "recipe_name":
+                    self.ingredients[key] = recipe_dict[key]
+
+
+    def dict_obj(self):
+        # Return a dict representation of Recipe, useful for JSON encoding
+        recipe_dict = {
+                'recipe_name': self.recipe_name,
+            }
+        for ingredient in self.ingredients:
+            recipe_dict[ingredient] = self.ingredients[ingredient]
+        return recipe_dict
+
     def __repr__(self):
         print_string = "\n" + self.recipe_name + ": \n"
         for ingr in self.ingredients:
             print_string += "    " + ingr + " - " + str(self.ingredients[ingr]) + "\n"
         return print_string
-        
-        
+
 
 class State:
     '''
@@ -56,11 +91,62 @@ class State:
     recipes = {}
     
     @staticmethod
+    def to_string():
+        print_me = ""
+        print_me += "Slot Inventory: " + str(State.inventory) + "\n"
+        print_me += "Ingredients: " + str(State.ingredients) + "\n"
+        print_me += "Recipes: " + str(State.recipes) + "\n"
+        return print_me
+    
+    @staticmethod
+    def process_request(request_type, request_data):
+        if request_type == "add_ingredients":
+            print("setting ingredient")
+            for new_in in request_data:
+                print("Checking if unique")
+                if new_in not in State.ingredients:
+                    print("it is unique")
+                    State.ingredients.append(new_in)
+            State.ingredients = sorted(State.ingredients, key=str.lower)
+            State.save_state()
+        elif request_type == "set_inventory":
+            State.inventory = request_data
+            State.save_state()
+        elif request_type == "add_recipes":
+            for recipe in request_data:
+                recipe_obj = Recipe(recipe_dict = recipe)
+                # If recipe exists update it, otherwise add to list
+                State.add_recipe(recipe_obj)
+        else:
+            print("Request type: " + request_type + " not recognized")
+    
+    @staticmethod
+    def clear_state():
+        # Should only be used for testing, deletes all variables
+        State.load_state()
+        # Save a copy
+        with open("backup"+State.saved_state_fn, "wb") as output:
+            pickle.dump(State.inventory, output)
+            pickle.dump(State.ingredients, output)
+            pickle.dump(State.recipes, output)
+        State.inventory = {
+            "slot_one": None,
+            "slot_two": None,
+            "slot_three": None,
+            "slot_four": None,
+        }
+        State.ingredients = []
+        State.recipes = {}
+    
+    @staticmethod
     def load_state():
-        with open(State.saved_state_fn, "rb") as input:
-            State.inventory = pickle.load(input)
-            State.ingredients = pickle.load(input)
-            State.recipes = pickle.load(input)
+        try:
+            with open(State.saved_state_fn, "rb") as input:
+                State.inventory = pickle.load(input)
+                State.ingredients = pickle.load(input)
+                State.recipes = pickle.load(input)
+        except FileNotFoundError:
+            pass
 
     @staticmethod
     def save_state():
--- drink_server.py
+++ drink_server.py
@@ -7,6 +7,7 @@ from pubnub_interface import Message_Handler
 import time
 import pickle
 import json
+from drink_helper import State, Recipe
 
 
 class Drink_Server:
@@ -14,7 +15,7 @@ class Drink_Server:
         # Should load saved settings which are stored in the static State class
         State.load_state()
         
-        self.message_handle = Message_Handler(intendedReceiver='client')
+        self.message_handle = Message_Handler(localId='server', intendedReceiver='client')
         self.run()
 
     def run(self):
@@ -23,33 +24,46 @@ class Drink_Server:
             if self.message_handle.message_queue:
                 # Loop through the received messages and process each one
                 while self.message_handle.message_queue:
-                    process_request(self.message_handle.message_queue.pop(0))
-                    time.sleep(5)
+                    self.process_request(self.message_handle.message_queue.pop(0))
+            
         # TODO READ INPUT FROM RASPBERRY PI
-        time.sleep(5)
+            print("server running")
+            print("State ingredients are " + str(State.ingredients))
+            print("State recipes are " + str(State.recipes))
+            print("State inventory is " + str(State.inventory))
+            time.sleep(3)
 
-    def process_request(data):
-        # Assume data is a python dictionary in JSON format
-        message = json.loads(data)
-        request_type = message['request_type']
-        request_data = message['request_data']
-        if request_type == "set_slot_data":
+    def process_request(self, data):
+        
+        request_sender = data[0]  # Sender is first element of tuple
+        request_type = data[1]  # Request_type is second element of tuple
+        request_data = data[2]  # Data is third element of tuple, should already be json decoded
+        
+        
+        # Getter methods that return the current State values to the requester
+        if request_type == "get_inventory":
+            self.message_handle.fireMessage(request_type="set_inventory",receiver=request_sender, jsonable_obj=State.inventory)
+            
+        elif request_type == "get_ingredients":
+            self.message_handle.fireMessage(request_type="set_ingredients",receiver=request_sender, jsonable_obj=State.ingredients)
+            
+        elif request_type == "get_recipes":
+            jsonable_recipes = [recipe.dict_obj() for name, recipe in State.recipes.items()]
+            self.message_handle.fireMessage(request_type="set_recipes", receiver=request_sender, jsonable_obj=jsonable_recipes)
+        
+        elif request_type == "make_drink":
             pass
-        if request_type == "get_slot_data":
-            pass
-        if request_type == "set_recipes":
-            pass
-        if request_type == "get_recipes":
-            pass
-        if request_type == "set_ingredients":
-            pass
-        if request_type == "get_ingredients":
-            pass
+        # These should be the setter methods such as set_ingredients, set_inventory, set_recipes
+        else:
+            State.process_request(request_type, request_data)
+        
+        
+        
         if request_type == "make_drink":
             pass
         
-    def send_request():
-        pass
+    def send_request(request_type, receiver, data_object):
+        self.message_handle.fireMessage(request_type=request_type, receiver=receiver, jsonable_obj=data_object)
     def send_drink_signals():
         pass
     def populate_lcd_screen():
